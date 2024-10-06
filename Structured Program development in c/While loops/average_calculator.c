e67197ef78e6a71dafe212d1e5cf11c9a8bf818c
DJ <DIVYJ913@GMAIL.COM>
2024-10-06 10:26:40 +0530

    Update average_calculator.c

--- Structured Program development in c/While loops/average_calculator.c
+++ Structured Program development in c/While loops/average_calculator.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
+int main(){
+    int total;
+    int grade;
+    int counter;
+    int average;
+    total = 0;
+    counter =1;
+    while (counter <=10){
+        printf("enter the grade of %dst student",counter);
+        scanf("%d",&grade);
+        counter = counter+1;
+        total = total+grade;
+    }
+    average = total/10;
+
+    printf("The average of the class of students is %d\n",average);
+
+    return 0;
+}
 
